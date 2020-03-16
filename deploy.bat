d347e7452d9e008d5431e407c29e68c488f3d6b6
Guillaume Fricker <fricker.g@zoho.com>
2020-03-16 12:36:07 +0100

    Streamlined deployment script for new LEP Server

--- deploy.bat
+++ deploy.bat
@@ -12,39 +12,19 @@
 :::
 
 @echo off
-if [%2] == [] goto:ttout
-if %2 == silent (
-	echo LEP Demonstrator Deployment Script
-	goto:main
-)
-
-:ttout
-@echo off
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do (
 	@echo(%%A
-	sleep 0.1
-)
-
-@echo off
-setlocal EnableDelayedExpansion
-for /f %%a in ('copy /Z "%~dpf0" nul') do set "CR=%%a"
-
-for /l %%n in (1,1,16) do (
-	call :spinner
-	sleep 0.1
 )
 
 if [%1]==[] goto:cancel
 
-:main
 cd ..
 if not exist lep-demonstrator (
 	call git clone git@gitlab.ti.bfh.ch:fricg2/lep-demonstrator.git
 )
 
-printf "\n\n1) RETRIEVING LATEST VERSION FROM REPOSITORY\n\n"
+echo 1) RETRIEVING LATEST VERSION FROM REPOSITORY
 
-sleep 1
 cd lep-demonstrator
 call git reset --hard
 call git fetch --all
@@ -61,31 +41,28 @@ if %1 == prod (
 call git checkout %BRANCH%
 call git reset --hard origin/%BRANCH%
 call git status
-sleep 1
-printf "\n2) INSTALLING PACKAGES\n"
 
-sleep 1
+echo 2) INSTALLING PACKAGES
+
 if exist build rmdir /s /q build
 if exist node_modules rmdir /s /q node_modules
 
 call npm install || exit /b -1
 
-sleep 1
-printf "\n3) BUILDING SOURCE CODE \n"
+echo 3) BUILDING SOURCE CODE
 call npm run build:%1 || exit /b -1
 
-sleep 1
-printf "\n4) PACKAGING WINDOWS EXECUTABLE \n"
+echo 4) PACKAGING WINDOWS EXECUTABLE
 call npm run package:%1 || exit /b -1
 
-printf "\n4) CREATING INSTALLER \n"
+echo 5) CREATING INSTALLER
 call npm run installer:%1 || exit /b -1
 goto:end
 
 :cancel
-echo 'ERROR!'
-echo 'the environment was not set correctly'
-echo 'check the parameter for %0 in your CI script!'
+echo ERROR!
+echo The environment was not set correctly
+echo Check the parameter for %0 in your CI script!
 exit /b -1
 
 :spinner
