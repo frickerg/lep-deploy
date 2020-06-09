eff77ea75d57787cefe033893ffbd49a0051893e
Guillaume Fricker <fricker.g@zoho.com>
2020-06-09 15:27:40 +0200

    Starting forever in the update script instead of deployment script

--- deploy.bat
+++ deploy.bat
@@ -61,9 +61,6 @@ if %1 == prod (
 ) else (
 	echo INFO: no need to create installer for %1
 )
-
-:: start the lepdemo daemon
-call forever start --uid "lepdemo" -c "npm run start:secure_server" -a ./
 goto:end
 
 :cancel
