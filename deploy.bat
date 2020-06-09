446015a223fda443cd0ae23bb8c53691c40a0c62
Guillaume Fricker <fricker.g@zoho.com>
2020-06-09 13:39:53 +0200

    Kill all node tasks before deployment

--- deploy.bat
+++ deploy.bat
@@ -20,6 +20,9 @@ for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do (
 :: stop all forever daemons
 call forever stopall
 
+:: kill all node tasks for good measure
+taskkill /f /im node.exe
+
 :: elevate to admin
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || ( echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
